9a099040a53e1477449edf3699d2ebeb7c20f026
danil8665 <danilfilatov319@gmail.com>
2023-03-25 23:36:16 +0200

    added main file

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ import { useRouter } from 'next/router';
 import { ButtonType } from 'antd/lib/button';
 import { stringify } from 'querystring';
 import list from 'antd/lib/transfer/list';
+import Main from "./main";
 
 
 const { Header, Sider, Content } = Layout;
@@ -183,141 +184,8 @@ const Home = () => {
 )
 
   return (
-      <>
-      {contextHolder}
-      {/* {onFinish} */}
-      <Layout className="layout" style={{ minHeight: '100vh' }}>
-      <Sider         
-      
-      collapsible
-      theme={theme} collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} breakpoint={'lg'} collapsedWidth={60}>
-        <Menu defaultSelectedKeys={['1']} mode='vertical'
-        theme={theme}
-        > 
-          <Menu.Item key={1}><Link href={'/'}><HomeOutlined/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Главная &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Link></Menu.Item>
-          <Menu.Item key={2}><SearchOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Категории &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Menu.Item>
-          <Menu.Item key={3}><UserOutlined/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Профиль &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Menu.Item>
-        </Menu>    
-       
-      </Sider>
-
-        <Layout className="site-layout">
-          <div className="homepage_wrap">
-          <div
-             className='site-layout-background' 
-          >
-             <Menu className='menuItems' selectable={false} theme={theme} mode="horizontal">
-          <div className='logo'>
-          <Menu.Item>
-              <Switch onChange={changeTheme} checked={theme === 'dark'} unCheckedChildren={<BulbFilled />} checkedChildren={<BulbOutlined/>}/>  Тема
-          </Menu.Item>
-          </div>
-             <div className='login'>
-              <Menu.Item>
-                 <Link href={'/logim'}>
-                  <Button type={type} icon={<LoginOutlined/>}>
-                    Вход
-                  </Button>
-                 </Link>
-              </Menu.Item>     
-
-                <Menu.Item>
-                 <Link href={'/logim'}>
-                  <Button type={type} icon={<PlusOutlined/>} style={{marginLeft: -20}}>
-                    Регистрация
-                  </Button>
-                 </Link>
-              </Menu.Item>       
-                </div>
-
-            </Menu>
-          </div>
-          <Content
-              className="site-layout-background"
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-              }}
-          >
-            <div className='wrap_home'>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 52 }} justify='space-around'>
-              <Col span={24}>
-              <div className='img_carousel'>
-                <Carousel autoplay effect='fade'>
-            <div>
-              <Image preview={false} src='3.webp'/>
-            </div>
-            <div>
-              <Image preview={false} src='2.webp' />
-            </div>              
-          </Carousel>
-          </div>
-              </Col>
-              <div className='loader'>
-              <LoadingOutlined style={{ fontSize: 24 }} spin />
-              </div>
-              <Map />     
-   
-            <FloatButton.BackTop />
-            </Row>
-
-            </div>
-          </Content>
-
-          <Footer style={{ textAlign: 'center' }}> 
-            Dixi Shoes ©2022 
-            <br/>
-            <div className="icons">
-            <Link href='https://www.instagram.com/dixi_zp/'>
-            <InstagramOutlined />
-            </Link>
-            &nbsp;
-            <Link href='https://m.facebook.com/profile.php?id=100063913516314'>
-            <FacebookOutlined href='/'/>
-            </Link>
-            <div className='qrCodes'>
-            {/* <QRCode size={200} icon={'instagram.svg.webp'} value="https://www.instagram.com/dixi_zp/" />
-            <QRCode value="https://www.instagram.com/dixi_zp/" /> */}
-            </div>
-            </div>
-          </Footer>
-          </div>
-        </Layout>
-      </Layout>
-      </>  
+    <Main/>
   )
 }
 
 export default Home;
-
-              {/* <Menu.Item>
-              <div className="search">
-                   <Search />
-                  </div> 
-              </Menu.Item>
-             
-                <div className='login'>
-                <Menu.Item>
-                  <div style={{marginTop: -17}}>
-                  <Link href={'/logim'}>
-                  <Button type={type} icon={<LoginOutlined />}>
-                   Войти
-                  </Button> 
-                  </Link>
-                  </div>
-                </Menu.Item>
-
-                <Menu.Item>
-                  <div style={{marginTop: -17, marginLeft: -25}}>
-                  <Link href={'/logim'}>
-                  <Button type={type} icon={<PlusOutlined />}>
-                    Регистрация
-                  </Button>
-                  </Link>
-                  </div>
-                </Menu.Item>
-                </div> */}
-
-function useAntdFormTable(list: any): { formProps: any; tableProps: any; paginationProps: any; } {
-  throw new Error('Function not implemented.');
-}
