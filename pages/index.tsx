5ecc89d2fa970ab3a4c8fcd9c5bfeeccafb12d4a
danil8665 <danilfilatov319@gmail.com>
2023-03-28 10:08:02 +0300

    link fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,7 +105,7 @@ const Home = () => {
        theme={theme}
         > 
           <Menu.Item key={1}><Link href={'/'}><HomeOutlined/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Главная </Link></Menu.Item>
-          <Menu.Item key={2}><Link href={'/addproduct'}><PlusOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Добавить товар </Link></Menu.Item>
+          <Menu.Item key={2}><Link href={'/addProduct'}><PlusOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Добавить товар </Link></Menu.Item>
           <Menu.Item key={3} onClick={() => openNotification('topRight')}><UserOutlined/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Профиль </Menu.Item>
         </Menu>    
       </Sider>
